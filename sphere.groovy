e20a0a489a0831a281422ff567276be6f0d5c804
robert <robert@robert-ThinkPad-T450s>
2022-05-07 14:24:59 -0400

    I added an extra spere and moved it to a differrent place
    practice and learning

--- sphere.groovy
+++ sphere.groovy
@@ -1,10 +1,13 @@
 //create a sphere
-CSG sphere = new Sphere(25)// Spheres radius
+CSG sphere = new Sphere(23)// Spheres radius
 				.toCSG()// convert to CSG to display
 				
-CSG sphereHighRes = new Sphere(25,// Spheres radius
-						40,//elevation Divisions
-						40)// vertical divisions
+CSG sphereHighRes = new Sphere(10,// Spheres radius
+						75,//elevation Divisions
+						30)// vertical divisions
 						.toCSG()// convert to CSG to display
+CSG sphere2 = new Sphere(23)// Spheres radius
+				.toCSG()// convert to CSG to display
+sphere2=sphere2.movex(-65)									
 sphere=sphere.movex(55)
-return [sphere,sphereHighRes]
+return [sphere,sphereHighRes,sphere2]
